6ac69cb21d231ed056a767aabcccb9c365ed8a03
Khalid Al Asad <katakgendong@gmail.com>
2022-03-12 08:28:17 +0700

    nyoba

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./Redux/Store/Store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./Styling/Components/Modals.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
